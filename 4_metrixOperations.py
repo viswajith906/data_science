5313a0c7706e5a7415c1b0d4d8e042bc3f6cc6cd
viswajith906 <viswajith906@gmail.com>
2025-11-07 10:56:52 +0530

    Create 4_metrixOperations.py

---
+++ 4_metrixOperations.py
@@ -0,0 +1,12 @@
+import numpy
+d1=numpy.matrix([[1,2,3],
+                 [4,5,6],
+                 [7,8,9]])
+d2=numpy.matrix([[1,2,3],
+                 [4,5,6],
+                 [7,8,9]])
+print(numpy.add(d1,d2))
+print(numpy.subtract(d1,d2))
+print(numpy.multiply(d1,d2))
+print(numpy.transpose(d1))
+print(d1*2)
