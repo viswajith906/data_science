ea26f0b2e2f71fdb4ef8cab6b63c44a693a43780
viswajith906 <viswajith906@gmail.com>
2025-11-11 06:41:29 +0530

    Update 4_metrixOperations.py

--- 4_metrixOperations.py
+++ 4_metrixOperations.py
@@ -1,12 +1,18 @@
-import numpy
-d1=numpy.matrix([[1,2,3],
-                 [4,5,6],
-                 [7,8,9]])
-d2=numpy.matrix([[1,2,3],
-                 [4,5,6],
-                 [7,8,9]])
-print(numpy.add(d1,d2))
-print(numpy.subtract(d1,d2))
-print(numpy.multiply(d1,d2))
-print(numpy.transpose(d1))
-print(d1*2)
+import numpy as np
+import numpy as pd
+
+def input_matrix(name):
+    print(f"\n enter element of matrix {name} (row wise) :")
+    matrix =[]
+    for i in range(3):
+        row=list(map(int,input(f"row {i+1} (3 numbers seperated by spaces):").split()))
+        matrix.append(row)
+    return np.array(matrix)
+A=input_matrix("A")
+B=input_matrix("B")
+scalar=int(input("\n enter scalar value:"))
+print("\n addition:\n",A+B)
+print("\n sub:\n",A-B)
+print("\n mul:\n",np.dot(A,B))
+print("\n scalar mul A by {scalar}:\n",A*scalar)
+print("\n transpose:\n",A.T)
